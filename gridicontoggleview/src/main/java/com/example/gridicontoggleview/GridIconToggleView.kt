ae180efef9ad07ad3cbc10bd9e9cee2f41beba8b
Anwesh43 <anweshthecool0@gmail.com>
2018-05-16 07:48:30 +0530

    adding GridIconToggleView

--- gridicontoggleview/src/main/java/com/example/gridicontoggleview/GridIconToggleView.kt
+++ gridicontoggleview/src/main/java/com/example/gridicontoggleview/GridIconToggleView.kt
@@ -3,3 +3,26 @@ package com.example.gridicontoggleview
 /**
  * Created by anweshmishra on 16/05/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.*
+
+class GridIconToggleView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+}
